87d512845756c800c3fef71b5d9d3f6b5fec790f
Bin <zzbsdh000@outlook.com>
2023-03-04 17:43:36 -0330

    Add start page

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,22 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const [intro, setIntro] = useState(true);
+  return (
+    <div className="App">
+      {intro ? (
+        <div className="intro-page">
+          <h3 className="title">Quizzical</h3>
+          <p className="info">Fun quiz</p>
+          <button className="start-btn" onClick={() => setIntro(false)}>
+            Start Quiz
+          </button>
+        </div>
+      ) : (
+        <div className="quiz-page">Quiz</div>
+      )}
+    </div>
+  );
 }
 
 export default App;
